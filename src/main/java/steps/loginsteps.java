992fcbee1d8d521881ebfe968e19ab6078a07fe4
Aziz <a.aouadi@coral-io.fr>
2022-12-09 11:07:50 +0100

    plusieurs sceniarios

--- src/main/java/steps/loginsteps.java
+++ src/main/java/steps/loginsteps.java
@@ -5,38 +5,77 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 public class loginsteps {
+    WebDriver driver ;
+
     @Given("user should navigate to the website")
     public void user_should_navigate_to_the_website() {
         try {
 
             WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver();
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.get("https://recette.uwas.fr/login");
             Thread.sleep(10000);
             String title = driver.getTitle();
             System.out.println(title);
+
+
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
 
-    @When("user write email and password and click on login")
-    public void user_write_email_and_password_and_click_on_login ()  {
+    @When("user write email as {string} and password as {string} and click on login")
+    public void user_write_email_and_password_and_click_on_login(String email , String password) {
+        try {
 
+            driver.findElement(By.id("normal_login_email")).sendKeys(email);
+            Thread.sleep(2000);
+            driver.findElement(By.id("normal_login_password")).sendKeys(password);
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div/form/div[6]/div/div/div/div/button/span")).click();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
 
     @Then("user should navigate to home page")
-    public void user_should_navigate_to_home_page () {
+    public void user_should_navigate_to_home_page() {
+        try {
 
+            Thread.sleep(5000);
+
+
+
+
+            }
+
+
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+    @Then("error message should appear")
+    public void error_message_should_appear() {
+        try {
+            Thread.sleep(2000);
+            System.out.println("error message");
+
+        }catch (InterruptedException e) {
+            throw new RuntimeException(e);
 
     }
-
+    }
 }
--- src/test/java/Features/login.feature
+++ src/test/java/Features/login.feature
@@ -3,8 +3,22 @@
 
   Feature: test the login functionality
 
-    Scenario: user to login with valid email and valid password
+      Scenario: user to login with valid email and valid password
+        Given user should navigate to the website
+        When user write email as "qaautomation@gmail.com" and password as "Aziz1996@" and click on login
+        Then user should navigate to home page
+
+
+    Scenario Outline: login with invalid credentials
       Given user should navigate to the website
-      When user write email and password and click on login
-      Then user should navigate to home page
+      When user write email as "<email>" and password as "<password>" and click on login
+      Then error message should appear
+
+      Examples:
+        | email            | password  |
+        | qa@gmail.com     | Admin123! |
+        | aaaa@outlook.fr  | Aziz1996@ |
+        | lllll@outlook.fr | Mo123!!   |
+
+
 

c63774a2e28dbe419bf76052e86bc06fbcb0cff4
Aziz <a.aouadi@coral-io.fr>
2022-12-20 15:08:23 +0100

    20/12/2022 automation cucumber selenium junit 4 features : sign up, login , forget password , add event

--- src/main/java/steps/add_event.java
+++ src/main/java/steps/add_event.java
@@ -16,9 +16,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import javax.swing.*;
 import java.sql.Date;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -48,7 +51,7 @@ public class add_event {
     @When("photographer should click on the button of add event")
     public void photographer_should_click_on_the_button_of_add_event() {
         try {
-            Thread.sleep(4000);
+            Thread.sleep(7000);
             driver.findElement(By.xpath("/html/body/div[1]/div/main/section[1]/div/div[1]/h1/button/span")).click();
 
 
@@ -123,8 +126,9 @@ public class add_event {
         }
     }
 
+
     @Then("title of event as {string} event in location as {string} at date as {string} is created")
-    public void title_of_event_event_in_location_at_date_as_is_created(String title_of_event , String location_of_event , String date_of_event) {
+    public void title_of_event_event_in_location_at_date_is_created(String title_of_event, String location_of_event, String date_of_event) {
         try {
             AtomicBoolean found = new AtomicBoolean(false);
             List<WebElement> eventNames = driver.findElements(By.className("eventDetails__name"));
@@ -132,12 +136,12 @@ public class add_event {
             List<WebElement> eventDate = driver.findElements(By.className("eventDetails__footer-date"));
             List<WebElement> eventStatus = driver.findElements(By.className("eventDetails__status"));
 
-            for (int i=0; i< eventNames.size(); i++) {
+            for (int i = 0; i < eventNames.size(); i++) {
                 boolean name = eventNames.get(i).getText().equals(title_of_event);
                 boolean location = eventLocation.get(i).getText().equals(location_of_event);
                 boolean date = eventDate.get(i).getText().equals(date_of_event);
                 boolean status = eventStatus.get(i).getText().equals("In progress");
-                if((name) && (location) && (date) && (status)) {
+                if ((name) && (location) && (date) && (status)) {
                     found.set(true);
                     break;
                 }
@@ -146,13 +150,14 @@ public class add_event {
             Thread.sleep(2000);
             driver.quit();
 
-        }catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
     }
+
     @Then("an error message appear under the title field")
-    public void an_error_message_appear_under_the_title_field () {
+    public void an_error_message_appear_under_the_title_field() {
         try {
             Assert.assertTrue(driver.findElement(By.id("name")).isDisplayed());
             Assert.assertTrue(driver.findElement(By.id("location")).isDisplayed());
@@ -160,13 +165,81 @@ public class add_event {
             Thread.sleep(2000);
             driver.quit();
 
-        }catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
 
     }
+
+    @Then("title of event as {string} event in location of event as {string} is created with the date added")
+    public void title_of_event_in_location_of_event_is_created_with_the_date_added(String title_of_event, String location_of_event) {
+        try {
+            boolean found = false;
+            List<WebElement> eventNames = driver.findElements(By.className("eventDetails__name"));
+            List<WebElement> eventLocation = driver.findElements(By.className("eventDetails__location"));
+            List<WebElement> eventDate = driver.findElements(By.className("eventDetails__footer-date"));
+            List<WebElement> eventStatus = driver.findElements(By.className("eventDetails__status"));
+            LocalDate localDate = LocalDate.now();
+            System.out.println("the date of today : " + localDate.toString());
+
+            for (int i = 0; i < eventNames.size(); i++) {
+                boolean name = eventNames.get(i).getText().equals(title_of_event);
+                boolean location = eventLocation.get(i).getText().equals(location_of_event);
+                boolean date = eventDate.get(i).getText().equals(localDate.toString());
+                boolean status = eventStatus.get(i).getText().equals("In progress");
+                if ((name) && (location) && (date) && (status)) {
+                    found = true;
+                    break;
+                }
+            }
+            System.out.println(found);
+            Assert.assertTrue(found);
+            Thread.sleep(2000);
+            driver.quit();
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Then("an error message appear and the event is created without image")
+    public void an_error_message_appear_and_the_event_is_created_without_image() {
+        try {
+            WebElement im = driver.findElement(By.className("ant-card-cover"));
+            im = im.findElement(By.tagName("img"));
+            Thread.sleep(3000);
+            System.out.println(im.getTagName());
+            String src_im = im.getAttribute("src");
+            System.out.printf(src_im);
+            boolean find = false ;
+            if (src_im.contentEquals("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW_80vVH0RghGLTxWZjz0EYc9JanOzT-m0wEUvdU0caY6bKU5n8oF5hbOHZlU9GVUM1dQ&usqp=CAU")){
+                find = true ;
+            }
+            Assert.assertTrue(find);
+            Thread.sleep(2000);
+            driver.quit();
+
+
+        }catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @And("photographer put a file in the image field for the event")
+    public void photographer_put_a_file_in_the_image_field_for_the_event() {
+        try {
+            Thread.sleep(3000);
+            WebElement source = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[2]/div[2]/form/div[5]/div/div[2]/div/div/span/div[1]/span/input"));
+            source.sendKeys("C://Users/DELL/Desktop/paiement.docx");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
 
 
      
--- src/test/java/Features/add_event.feature
+++ src/test/java/Features/add_event.feature
@@ -10,7 +10,7 @@ Feature: test add event functionality
     And photographer should click on the button ok
     Then title of event as "noel" event in location as "paris" at date as "2022-12-25" is created
 
-  Scenario Outline: photographer can login and add event without filling the title of event
+  Scenario Outline: photographer can login and can't add event without filling the title of event
     Given photographer should login
     When photographer should click on the button of add event
     And photographer should fill the title of event as "<title_of_event>"
@@ -26,5 +26,37 @@ Feature: test add event functionality
       |                | amesterdam        |               |
       |                |                   | 2022-12-18    |
 
+  Scenario Outline: photographer can login and add event without filling the location of event
+    Given photographer should login
+    When photographer should click on the button of add event
+    And photographer should fill the title of event as "<title_of_event>"
+    And photographer should fill the location of event as "<location_of_event>"
+    And photographer should fill the date of event as "<date_of_event>"
+    And photographer put an image for the event
+    And photographer should click on the button ok
+    Then title of event as "<title_of_event>" event in location as "<location_of_event>" at date as "<date_of_event>" is created
 
+    Examples:
+      | title_of_event | location_of_event | date_of_event |
+      | formation      |                   | 2022-12-24    |
+      | weed party     |                   | 2022-12-31    |
+      | tommorowland   |                   | 2023-01-01    |
 
+  Scenario: photographer can login and add event without filling the date of event
+    Given photographer should login
+    When photographer should click on the button of add event
+    And photographer should fill the title of event as "noel"
+    And photographer should fill the location of event as "bruxelle"
+    And photographer put an image for the event
+    And photographer should click on the button ok
+    Then title of event as "noel" event in location of event as "bruxelle" is created with the date added
+
+  Scenario: photographer can login and can't add event with filling a file not image type in the image field
+    Given photographer should login
+    When photographer should click on the button of add event
+    And photographer should fill the title of event as "curva"
+    And photographer should fill the location of event as "rades"
+    And photographer should fill the date of event as "2022-06-19"
+    And photographer put a file in the image field for the event
+    And photographer should click on the button ok
+    Then an error message appear and the event is created without image

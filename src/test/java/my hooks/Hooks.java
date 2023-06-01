01fe51828d8d69cf964c9100045f7d325683f370
unknown <a.aouadi@coral-io.fr>
2023-06-01 15:08:36 +0100

    update code with new IDS

--- src/test/java/my hooks/Hooks.java
+++ src/test/java/my hooks/Hooks.java
@@ -1,14 +1,14 @@
-import cucumber.api.java.Before;
-import cucumber.api.java.After;
+// import cucumber.api.java.Before;
+// import cucumber.api.java.After;
 
-public class Hooks {
-    @Before
-    public void before(){
-        System.out.println("before");
+// public class Hooks {
+//     @Before
+//     public void before(){
+//         System.out.println("before");
     
-    }
-    @After
-    public void after(){
-        System.out.println("after");
-    }
-}
+//     }
+//     @After
+//     public void after(){
+//         System.out.println("after");
+//     }
+// }
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -12,7 +12,7 @@ import org.uwas.Driver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/functionalFeatures/2login"
+        features = "src/test/resources/functionalFeatures"
         ,glue = "stepDefinitions",
 
 

acd5f81f25e9fa3f3cb7abf97f5e2f4d887eef97
unknown <a.aouadi@coral-io.fr>
2023-06-02 12:35:40 +0100

    update code with new IDS

--- src/main/java/org/uwas/Driver.java
+++ src/main/java/org/uwas/Driver.java
@@ -86,7 +86,6 @@ public class Driver {
                 options.addArguments("--remote-allow-origins=*");
                 this.webDriver = new ChromeDriver(options);
                 this.webDriver.manage().window().maximize();
-                this.webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
                 
              

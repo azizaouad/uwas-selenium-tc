8bc91eeb8d317636f2094718feb4adc0769ef02c
azizaouad <72755717+azizaouad@users.noreply.github.com>
2023-06-13 10:17:46 +0100

    adding edit event feature

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
     features = "src/test/resources/functionalFeatures/",
     
+    
     glue = "stepDefinitions",
 
     plugin = { "pretty", "html:target/reports/report.html","json:target/reports/report.json" }

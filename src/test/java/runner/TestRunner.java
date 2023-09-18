41c346081dbf08718d05cd0798f08c53682a9466
azizaouad <72755717+azizaouad@users.noreply.github.com>
2023-09-18 15:44:31 +0100

    add wait for visibility of element function in all features

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/functionalFeatures/scenario.feature", glue = "stepDefinitions", plugin = {
+@CucumberOptions(features = "src/test/resources/functionalFeatures/", glue = "stepDefinitions", plugin = {
                 "pretty",
                 "html:target/reports/report.html", "json:target/reports/report.json" }
 
